076e84e603331fb945642850660a0750cd30ff31
Fernweh99 <daniele.dimento@gmail.com>
2022-09-20 14:34:14 +0200

    create structure table train with migration

--- database/migrations/2022_09_20_115933_create_train_table.php
+++ database/migrations/2022_09_20_115933_create_train_table.php
@@ -15,6 +15,15 @@ class CreateTrainTable extends Migration
     {
         Schema::create('train', function (Blueprint $table) {
             $table->id();
+            $table->string('azienda', 50);
+            $table->string('stazione_di_partenza', 50);
+            $table->string('stazione_di_arrivo', 50);
+            $table->time('orario_di_partenza');
+            $table->time('orario_di_arrivo');
+            $table->string('codice_treno', 15);
+            $table->string('numero_carrozze', 2);
+            $table->boolean('in_orario');
+            $table->boolean('cancellato');
             $table->timestamps();
         });
     }
