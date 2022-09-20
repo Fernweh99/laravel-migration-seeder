debf7015196bcb985c9ac9f57d375e783af14aa2
Fernweh99 <daniele.dimento@gmail.com>
2022-09-20 16:21:10 +0200

    finish ex with use faker

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -2,31 +2,35 @@
 
 @section('content-main')
   <div class="container">
-    @foreach ($trains as $train)    
-    <div class="card m-5" style="width: 22rem">
-      <div class="card-body">
-        <div class="container-fluid">
-          <div class="row">
-            <div class="col-8">
-              <h5 class="card-title">{{$train['azienda']}}</h5>
-              <h6 class="card-subtitle mb-2 text-muted">Da: <strong>{{$train['stazione_di_partenza']}}</strong> a: <strong>{{$train['stazione_di_arrivo']}}</strong></h6>
-              <div class="card-text">
-                <span>Cod Treno: {{$train['codice_treno']}}</span><br/>
-                <span> Numero Carrozze: {{$train['numero_carrozze']}}</span>
+    <div class="row">
+      @foreach ($trains as $train)  
+      <div class="col-4">
+        <div class="card m-5" style="width: 22rem">
+          <div class="card-body">
+            <div class="container-fluid">
+              <div class="row">
+                <div class="col-8">
+                  <h5 class="card-title">{{$train['azienda']}}</h5>
+                  <h6 class="card-subtitle mb-2 text-muted">Da: <strong>{{$train['stazione_di_partenza']}}</strong> a: <strong>{{$train['stazione_di_arrivo']}}</strong></h6>
+                  <div class="card-text">
+                    <span>Cod Treno: {{$train['codice_treno']}}</span><br/>
+                    <span> Numero Carrozze: {{$train['numero_carrozze']}}</span>
+                  </div>
+                </div>
+                <div class="col" style="font-size: 12px">
+                  <p>
+                    <span>partenza:</span><br/>
+                    <span>{{$train['orario_di_partenza']}}</span>
+                  </p>
+                  <span>arrivo:</span><br/>
+                  <span>{{$train['orario_di_arrivo']}}</span>
+                </div>
               </div>
-            </div>
-            <div class="col" style="font-size: 12px">
-              <p>
-                <span>partenza:</span><br/>
-                <span>{{$train['orario_di_partenza']}}</span>
-              </p>
-              <span>arrivo:</span><br/>
-              <span>{{$train['orario_di_arrivo']}}</span>
             </div>
           </div>
         </div>
-      </div>
+      </div>  
+      @endforeach
     </div>
-    @endforeach
   </div>
 @endsection
